747dca1342c678fb148726878f6460ffbfbc9aa2
Akhmdly <ehmedli.aynur555@gmail.com>
2023-12-14 03:43:09 +0400

    backend1

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+fs.readFile("./data.json",'utf8',(err,data)=>{
+    if(!err){
+            app.get('/',(req,res)=>{
+            res.send(data)
+            console.log(data)
+        }) 
+    }
+    else{
+        console.log(err);
+    }
+})
+app.listen(3000,()=>{
+    console.log('Sever çalışdı.');
+})
